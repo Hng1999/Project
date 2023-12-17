6edea9251d081380057ea33dfa494c41d6fc5c5e
Hng1999 <109809743+Hng1999@users.noreply.github.com>
2023-12-17 20:45:22 +0800

    Add files via upload

--- EXPRESS TIME WEBSITE/EXPRESSTIME(NewVersion).js
+++ EXPRESS TIME WEBSITE/EXPRESSTIME(NewVersion).js
@@ -4,6 +4,9 @@ let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
+let adminBtn = document.querySelector('#admin-login-btn')
+let adminloginForm = document.querySelector('.admin-login-form-container');
+let adminformClose = document.querySelector('#admin-form-close');
 
 searchBtn.addEventListener('click', () => {
     searchBtn.classList.toggle('fa-search');
@@ -13,3 +16,6 @@ searchBtn.addEventListener('click', () => {
 
 formBtn.addEventListener('click', () => { loginForm.classList.add('active'); });
 formClose.addEventListener('click', () => { loginForm.classList.remove('active'); });
+
+adminBtn.addEventListener('click', () => { adminloginForm.classList.add('active'); });
+adminformClose.addEventListener('click', () => { adminloginForm.classList.remove('active'); });
