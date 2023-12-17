1767cfc36e40c6d011c0b720ce939fa2da3ee948
Hng1999 <109809743+Hng1999@users.noreply.github.com>
2023-12-17 18:27:49 +0800

    Add files via upload

---
+++ EXPRESS TIME WEBSITE/EXPRESSTIME(NewVersion).js
@@ -0,0 +1,15 @@
+// JavaScript source code
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+searchBtn.addEventListener('click', () => {
+    searchBtn.classList.toggle('fa-search');
+    searchBtn.classList.toggle('fa-xmark');
+    searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () => { loginForm.classList.add('active'); });
+formClose.addEventListener('click', () => { loginForm.classList.remove('active'); });
