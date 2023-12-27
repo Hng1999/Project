1cec4ca6c70251f36b5821d26723fbf2c0aa5834
Hng1999 <109809743+Hng1999@users.noreply.github.com>
2023-12-27 18:58:00 +0800

    Add files via upload

--- Test3/Test3.js
+++ Test3/Test3.js
@@ -40,4 +40,25 @@ $(document).ready(function () {
     });
 });
 
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
 
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
