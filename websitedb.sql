9836bf5189d31064f819298a82f46a3bf3860407
Izamudin <149231860+Izamudin@users.noreply.github.com>
2024-01-07 15:07:15 +0800

    Add files via upload

--- websitedb.sql
+++ websitedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 01, 2024 at 02:41 PM
+-- Generation Time: Jan 07, 2024 at 08:06 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -46,10 +46,20 @@ CREATE TABLE `timetable` (
 
 INSERT INTO `timetable` (`No`, `BusType`, `RouteID`, `PlateNo`, `WDFirstBus`, `WDLastBus`, `WEFirstBus`, `WELastBus`, `Frequency`, `Status`) VALUES
 (1, 'RapidKL Bus', '300', 'WES1245', '05:20:00', '22:56:00', '05:20:00', '22:56:00', '6', 'OK'),
+(6, 'RapidKL Bus', '302', 'WQE6582', '06:00:00', '23:30:00', '06:00:00', '23:30:00', '20-30', 'OK'),
+(7, 'RapidKL Bus', '303', 'WDE4353', '05:30:00', '23:00:00', '05:30:00', '23:00:00', '10', 'OK'),
 (4, 'GoKL Bus', 'GOKL-01-(GREEN)', 'WER 2356', '06:00:00', '23:00:00', '07:00:00', '23:00:00', '10-15', 'OK'),
+(12, 'GoKL Bus', 'GOKL-02(PURPLE)', 'WEV5492', '06:00:00', '23:00:00', '07:00:00', '23:00:00', '10-15', 'OK'),
+(13, 'GoKL Bus', 'GOKL-03-(RED)', 'WBG8462', '06:00:00', '23:00:00', '07:00:00', '23:00:00', '10-15', 'OK'),
 (5, 'Smart Selangor Bus', 'HS01', 'WSR 3232', '06:00:00', '20:00:00', '06:00:00', '20:00:00', '', 'OK'),
-(2, 'MRT Putrajaya Feeder Bus', 'T103', '[WXC4567]', '06:00:00', '23:30:00', '06:00:00', '11:30:00', '20-40', 'OK'),
-(3, 'MRT Kajang Feeder Bus', 'T305', 'WJX 5455', '06:00:00', '23:30:00', '06:00:00', '23:30:00', '30-50', 'OK');
+(14, 'Smart Selangor Bus', 'KLG1', 'WVK7635', '06:00:00', '22:00:00', '06:00:00', '22:00:00', '15', 'OK'),
+(15, 'Smart Selangor Bus', 'KS01', 'WSC5382', '06:20:00', '19:45:00', '06:20:00', '19:45:00', '70-135', 'OK'),
+(2, 'MRT Putrajaya Feeder Bus', 'T103', 'WXC4567', '06:00:00', '23:30:00', '06:00:00', '11:30:00', '20-40', 'OK'),
+(10, 'MRT Putrajaya Feeder Bus', 'T106', 'WGH7865', '06:00:00', '23:30:00', '06:00:00', '23:30:00', '30-45', 'OK'),
+(11, 'MRT Putrajaya Feeder Bus', 'T107', 'WRT9803', '06:00:00', '23:30:00', '06:00:00', '23:30:00', '30-45', 'OK'),
+(3, 'MRT Kajang Feeder Bus', 'T305', 'WJX 5455', '06:00:00', '23:30:00', '06:00:00', '23:30:00', '30-50', 'OK'),
+(8, 'MRT Kajang Feeder Bus', 'T352', 'WFG5482', '05:45:00', '23:30:00', '05:45:00', '23:30:00', '15-48', 'OK'),
+(9, 'MRT Kajang Feeder Bus', 'T400', 'WDR4928', '06:00:00', '23:30:00', '06:00:00', '23:30:00', '30-50', 'OK');
 
 -- --------------------------------------------------------
 
@@ -84,8 +94,17 @@ INSERT INTO `train_status` (`train_id`, `train_name`, `frequency`, `status`) VAL
 
 CREATE TABLE `user` (
   `Username` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL
+  `Password` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`Username`, `Password`, `Email`) VALUES
+('user1', '12345', 'user1@gmail.com'),
+('user2', '12346', 'user2@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -102,6 +121,12 @@ ALTER TABLE `timetable`
 --
 ALTER TABLE `train_status`
   ADD PRIMARY KEY (`train_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
